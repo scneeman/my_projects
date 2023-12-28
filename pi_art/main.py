1d9f7c7e57956782d1b7a81b2075c0c34eb2e4d5
me_chromebook <heyscottn@gmail.com>
2023-12-28 16:33:03 -0600

    edits

--- pi_art/main.py
+++ pi_art/main.py
@@ -3,3 +3,22 @@
 # arrange into tuples of 3
 # create 2d list of colors
 
+# define any functions
+def get_pi():
+    f = open("pi_art/pi_million_digits.txt", "r")  # open the file for reading
+    pi = f.read() # read the data into one long string
+    f.close() # close the file to release memory
+    # TODOclean up
+    pi = pi.replace(".", "")
+    pi = pi.replace("\n", "")
+    pi = pi.replace(" ", "")
+    return pi
+
+pi = "314159"
+cols = []
+for digit in pi:
+
+
+# call the get_pi function
+# pi = get_pi()
+# print(len(pi))
